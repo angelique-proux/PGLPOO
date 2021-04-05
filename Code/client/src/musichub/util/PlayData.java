b59e6d81e6de783ac014fb420f1abe380e70e705
Angélique P <72751313+angelique2000@users.noreply.github.com>
2021-04-05 15:23:18 +0200

    Update PlayData.java

--- Code/client/src/musichub/util/PlayData.java
+++ Code/client/src/musichub/util/PlayData.java
@@ -5,43 +5,43 @@ import java.io.IOException;
 
 public class PlayData implements PlayDataInterface {
     AudioInputStream musicToListenTo;
-    SourceDataLine line = null;
+    SourceDataLine line;
     boolean stopMusic = false;
+    byte[] samples = new byte[4096];
 
-    public PlayData(AudioInputStream musicToListenTo) {
-        this.musicToListenTo =musicToListenTo;
-        this.initialiseLine();
+    public PlayData(AudioInputStream stream, SourceDataLine line) {
+        this.musicToListenTo = stream;
+        this.line = line;
     }
 
-    @Override
+    public void setStopMusic(boolean stop) {
+        if (stop == true) {
+            this.stopMusic = true;
+        } else {
+            this.stopMusic = false;
+        }
+    }
+
     public void playTheMusic(){
-        int BUFFER_SIZE = 4096; // 4K buffer
         try {
+            stopMusic = false;
             line.open(musicToListenTo.getFormat());
             line.start();
-            byte[] samples = new byte[BUFFER_SIZE];
             int count;
-            while ((count = musicToListenTo.read(samples, 0, BUFFER_SIZE)) != -1) {
+            musicToListenTo.reset();
+            while ((count = musicToListenTo.read(samples, 0, samples.length)) != -1) {
                 line.write(samples, 0, count);
+                if(stopMusic == true) {
+                    musicToListenTo.mark(10);
+                    break;
+                }
             }
+            line.drain();
+            line.stop();
         }
         catch (IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
-    }
-
-    public void initialiseLine() {
-        try {
-            AudioFormat audioFormat = musicToListenTo.getFormat();
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-            line = (SourceDataLine) AudioSystem.getLine(info);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void closeMusic() {
-        stopMusic = true;
     }
 
 }
