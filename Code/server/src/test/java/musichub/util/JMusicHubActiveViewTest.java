ccd061108cd84ce6052f64ee99973f0f8696f147
gael-lejeune <lejeune.gael@free.fr>
2021-04-28 17:06:08 +0200

    Implemented first test (not complete)

---
+++ Code/server/src/test/java/musichub/util/JMusicHubActiveViewTest.java
@@ -0,0 +1,27 @@
+package jmusichub.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import musichub.main.*;
+import musichub.util.*;
+import musichub.business.*;
+
+class JMusicHubActiveViewTest {
+	@Test
+	void TestValidator() {
+
+		// PasswordValidator pv = new PasswordValidator();
+		// //less than 6
+		// assertEquals(pv.validatePassword("12345"), false);
+		// //exactly 6
+		// assertEquals(pv.validatePassword("123456"), true);
+		// //more than 20
+		// assertEquals(pv.validatePassword("123456abcdeo pç0°)345lskdfdllke23"), false);
+		// //exactly 20
+		// assertEquals(pv.validatePassword("123456abcdeo pç0°)34"), true);
+		// //no number
+		// assertEquals(pv.validatePassword("abc$deo pç°)"), false);
+	}
+
+}
